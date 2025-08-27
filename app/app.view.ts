8fe456210d2cda9c25defb83fdee4305dd2cb6ff
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-08-27 16:29:29 +0300

    static svg

--- app/app.view.ts
+++ app/app.view.ts
@@ -11,34 +11,5 @@ namespace $.$$ {
 			})
 		}
 		
-		// эту логику я бы добавил в какой нибудь из корневых компонентов или 
-		// как плагин, что бы можно было во вью три передать иконку для сайта
-		Favicon() {
-			this.apply_favicon()
-			return super.Favicon()
-		}
-
-		@$mol_mem
-		favicon_data() {
-			const path = new $.$mol_icon_bird().path()
-			const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="${path}"/></svg>`
-			return 'data:image/svg+xml,' + encodeURIComponent(svg)
-		}
-
-		@$mol_mem
-		apply_favicon() {
-			this.$.$mol_state_time.now(0)
-
-			const doc = this.$.$mol_dom_context.document
-			let link = doc.querySelector('link[rel="icon"]') as HTMLLinkElement | null
-			if (!link) {
-				link = doc.createElement('link')
-				link.rel = 'icon'
-				doc.head.appendChild(link)
-			}
-			link.type = 'image/svg+xml'
-			link.href = this.favicon_data()
-			return null as any
-		}
 	}
 }
